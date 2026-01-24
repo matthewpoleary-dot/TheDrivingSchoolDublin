17d61e940af42278121b354f9b019a68d25fb2ea
Matthew O'Leary <matthewpoleary@gmail.com>
2026-01-24 18:54:25 +0000

    chore: disable react/no-unescaped-entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Allow normal apostrophes/quotes in JSX text like "we'll", "instructor's", etc.
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
